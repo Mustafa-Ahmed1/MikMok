f46fb86d0b0a18e55c1e637f183e272162ab346d
Wesam Nabeel <wesamezio@gmail.com>
2022-07-31 01:23:53 +0300

    edit: add home fragment

---
+++ app/src/main/java/com/frenchfriesclan/mikmok/ui/home/HomeFragment.kt
@@ -0,0 +1,21 @@
+package com.frenchfriesclan.mikmok.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.frenchfriesclan.mikmok.R
+import com.frenchfriesclan.mikmok.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
