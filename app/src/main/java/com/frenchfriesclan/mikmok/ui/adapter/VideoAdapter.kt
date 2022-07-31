a7c05315bf8f5f2a5ea7613ffeefb8b594207610
Salah-Kafaji <salah94master@gmail.com>
2022-07-31 07:40:34 +0300

    initial commit

--- app/src/main/java/com/frenchfriesclan/mikmok/ui/adapter/VideoAdapter.kt
+++ app/src/main/java/com/frenchfriesclan/mikmok/ui/adapter/VideoAdapter.kt
@@ -1,4 +1,33 @@
 package com.frenchfriesclan.mikmok.ui.adapter
 
-class VideoAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.frenchfriesclan.mikmok.model.domain.Video
+
+class VideoAdapter(private val list: List<Video>): RecyclerView.Adapter<VideoAdapter.BaseViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+
+    abstract class BaseViewHolder(viewItem: View) : RecyclerView.ViewHolder(viewItem) {
+
+    }
+
+    class VideoViewHolder(viewItem: View) : BaseViewHolder(viewItem) {
+//        val binding = ItemVideoBinding.bind(viewItem)
+    }
+
+
 }
