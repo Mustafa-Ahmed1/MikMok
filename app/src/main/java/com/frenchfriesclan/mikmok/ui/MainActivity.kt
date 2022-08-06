781987d86c04bcd6190ec1f86cbbbb0419a48ae6
Wesam Nabeel <wesamezio@gmail.com>
2022-08-06 14:23:32 +0300

    feat: add splash screen and icon for the app

--- app/src/main/java/com/frenchfriesclan/mikmok/ui/MainActivity.kt
+++ app/src/main/java/com/frenchfriesclan/mikmok/ui/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.frenchfriesclan.mikmok.ui
 
+import android.os.Bundle
+import com.frenchfriesclan.mikmok.R
 import com.frenchfriesclan.mikmok.databinding.ActivityMainBinding
 import com.frenchfriesclan.mikmok.ui.base.BaseActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun bindingInflater() = ActivityMainBinding.inflate(layoutInflater)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_MikMok)
+    }
 }
